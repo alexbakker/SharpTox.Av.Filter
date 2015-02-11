d954e33b1c15557915e32343cfdfa5704f113b8f
Impyy <impyy@tox.im>
2015-02-12 00:08:45 +0100

    Hopefully fix formatting, silly monodevelop...

--- SharpTox.Av.Filter/FilterAudio.cs
+++ SharpTox.Av.Filter/FilterAudio.cs
@@ -2,28 +2,28 @@
 
 namespace SharpTox.Av.Filter
 {
-    /// <summary>
-    /// Represents an instance of filter_audio.
-    /// </summary>
-    public class FilterAudio : IDisposable
-    {
-        private bool _disposed;
-        private int _sampleRate;
-        private readonly FilterAudioHandle _filterAudio;
+	/// <summary>
+	/// Represents an instance of filter_audio.
+	/// </summary>
+	public class FilterAudio : IDisposable
+	{
+		private bool _disposed;
+		private int _sampleRate;
+		private readonly FilterAudioHandle _filterAudio;
 
-        /// <summary>
-        /// The amount of time in ms it takes for audio to be played and recorded back after.
-        /// </summary>
-        public short EchoDelay
-        {
-            set
-            {
-                if (_disposed)
-                    throw new ObjectDisposedException(GetType().FullName);
+		/// <summary>
+		/// The amount of time in ms it takes for audio to be played and recorded back after.
+		/// </summary>
+		public short EchoDelay
+		{
+			set
+			{
+				if (_disposed)
+					throw new ObjectDisposedException(GetType().FullName);
 
-                FilterAudioFunctions.SetEchoDelayMs(_filterAudio, value);
-            }
-        }
+				FilterAudioFunctions.SetEchoDelayMs(_filterAudio, value);
+			}
+		}
 
 		private bool _echoFilterEnabled = true;
 		private bool _voiceFilterEnabled = true;
@@ -101,64 +101,66 @@ namespace SharpTox.Av.Filter
 			}
 		}
 
-        /// <summary>
-        /// Initialises a new instance of filter_audio.
-        /// </summary>
-        /// <param name="sampleRate"></param>
-        public FilterAudio(int sampleRate)
-        {
-            _sampleRate = sampleRate;
-            _filterAudio = FilterAudioFunctions.NewFilterAudio((uint)sampleRate);
-        }
+		/// <summary>
+		/// Initialises a new instance of filter_audio.
+		/// </summary>
+		/// <param name="sampleRate"></param>
+		public FilterAudio(int sampleRate)
+		{
+			_sampleRate = sampleRate;
+			_filterAudio = FilterAudioFunctions.NewFilterAudio((uint)sampleRate);
+		}
 
-        /// <summary>
-        /// Filters audio. (highpass filter, gain control)
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="samples">the amount of samples per frame</param>
-        /// <returns></returns>
-        public bool Filter(short[] data, int samples)
-        {
-            if (_disposed)
-                throw new ObjectDisposedException(GetType().FullName);
+		/// <summary>
+		/// Filters audio. (highpass filter, gain control)
+		/// </summary>
+		/// <param name="data"></param>
+		/// <param name="samples">the amount of samples per frame</param>
+		/// <returns></returns>
+		public bool Filter(short[] data, int samples)
+		{
+			if (_disposed)
+				throw new ObjectDisposedException(GetType().FullName);
 
-            return FilterAudioFunctions.FilterAudio(_filterAudio, data, (uint)samples) == 0;
-        }
+			return FilterAudioFunctions.FilterAudio(_filterAudio, data, (uint)samples) == 0;
+		}
 
-        /// <summary>
-        /// Makes sure echo gets canceled from the frame.
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="samples">the amount of samples per frame</param>
-        /// <returns></returns>
-        public bool PassAudioOutput(short[] data, int samples)
-        {
-            if (_disposed)
-                throw new ObjectDisposedException(GetType().FullName);
+		/// <summary>
+		/// Makes sure echo gets canceled from the frame.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <param name="samples">the amount of samples per frame</param>
+		/// <returns></returns>
+		public bool PassAudioOutput(short[] data, int samples)
+		{
+			if (_disposed)
+				throw new ObjectDisposedException(GetType().FullName);
 
-            return FilterAudioFunctions.PassAudioOutput(_filterAudio, data, (uint)samples) == 0;
-        }
+			return FilterAudioFunctions.PassAudioOutput(_filterAudio, data, (uint)samples) == 0;
+		}
 
-        /// <summary>
-        /// Releases all resources used by this instance of filter_audio.
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+		/// <summary>
+		/// Releases all resources used by this instance of filter_audio.
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
-        private void Dispose(bool disposing)
-        {
-            if (_disposed)
-                return;
+		private void Dispose(bool disposing)
+		{
+			if (_disposed)
+				return;
 
-            if (disposing) { }
+			if (disposing)
+			{
+			}
 
-            if (!_filterAudio.IsInvalid && !_filterAudio.IsClosed && _filterAudio != null)
-                _filterAudio.Dispose();
+			if (!_filterAudio.IsInvalid && !_filterAudio.IsClosed && _filterAudio != null)
+				_filterAudio.Dispose();
 
-            _disposed = true;
-        }
-    }
+			_disposed = true;
+		}
+	}
 }
--- SharpTox.Av.Filter/FilterAudioFunctions.cs
+++ SharpTox.Av.Filter/FilterAudioFunctions.cs
@@ -3,26 +3,26 @@ using System.Runtime.InteropServices;
 
 namespace SharpTox.Av.Filter
 {
-    internal static class FilterAudioFunctions
-    {
-        const string dllName = "filter_audio.dll";
+	internal static class FilterAudioFunctions
+	{
+		const string dllName = "filter_audio.dll";
 
-        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_filter_audio")]
-        internal static extern FilterAudioHandle NewFilterAudio(uint fs);
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_filter_audio")]
+		internal static extern FilterAudioHandle NewFilterAudio(uint fs);
 
-        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kill_filter_audio")]
-        internal static extern void KillFilterAudio(IntPtr filterAudio);
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kill_filter_audio")]
+		internal static extern void KillFilterAudio(IntPtr filterAudio);
 
-        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "filter_audio")]
-        internal static extern int FilterAudio(FilterAudioHandle filterAudio, short[] data, uint samples);
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "filter_audio")]
+		internal static extern int FilterAudio(FilterAudioHandle filterAudio, short[] data, uint samples);
 
-        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pass_audio_output")]
-        internal static extern int PassAudioOutput(FilterAudioHandle filterAudio, short[] data, uint samples);
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pass_audio_output")]
+		internal static extern int PassAudioOutput(FilterAudioHandle filterAudio, short[] data, uint samples);
 
-        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "set_echo_delay_ms")]
-        internal static extern int SetEchoDelayMs(FilterAudioHandle filterAudio, short msInSndCardBuf);
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "set_echo_delay_ms")]
+		internal static extern int SetEchoDelayMs(FilterAudioHandle filterAudio, short msInSndCardBuf);
 
 		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "enable_disable_filters")]
 		internal static extern int EnableDisableFilters(FilterAudioHandle filterAudio, int echo, int voice, int grain);
-    }
+	}
 }
